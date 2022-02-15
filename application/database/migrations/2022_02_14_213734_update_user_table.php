73257c97e8eb289d86048321920c2dd38eee3aec
fjsho <gorimura7@gmail.com>
2022-02-15 20:16:10 +0900

    [add]ユーザーテーブル用マイグレーションファイルの追加/修正

---
+++ application/database/migrations/2022_02_14_213734_update_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('self_introduction', 160)->nullable();
+            $table->string('profile_pic_path', 255)->nullable()->unique();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique(['profile_pic_path']);
+            $table->dropColumn(['self_introduction','profile_pic_path', 'deleted_at']);
+        });
+    }
+}
