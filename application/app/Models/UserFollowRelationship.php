81902a7912554665174885d620f3c73d480e21f0
fjsho <gorimura7@gmail.com>
2022-02-16 22:14:10 +0900

    [fix]リレーションテーブルのモデル実装

--- application/app/Models/UserFollowRelationship.php
+++ application/app/Models/UserFollowRelationship.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class UserFollowRelationship extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'following_id',
+        'followed_id',
+    ];
+
+    /**
+     * モデルにタイムスタンプを付けるか
+     *
+     * @var bool
+     */
+    //このテーブルは中間テーブルであり、フォローの有無（=レコードの有無）を判定できればいいのでfalseとした
+    public $timestamps = false; 
+
 }
