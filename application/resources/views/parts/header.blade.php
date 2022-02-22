dd6101dec5abbc66877e23d88ede8d1802b77fb1
Daisuke <a1485@icloud.com>
2022-02-22 13:41:26 +0900

    [add]headerテンプレートの作成

---
+++ application/resources/views/parts/header.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg navbar-light pl-5 pr-5 pt-2 pb-2">
+  <a class=" logo navbar-brand" href="/">学習レシピ</a>
+  <div class="collapse navbar-collapse" id="Navber">
+    <ul class="navbar-nav ml-auto mr-5">
+      <li class="nav-item ml-2">
+        <button class="  nav-link text-white" href="#">自分の記事</button>
+      </li>
+      <li class="nav-item ml-2">
+        <button class="nav-link text-white" id="post-link" href="/drafts/new">プロフィール</button>
+      </li>
+      <li class="nav-item ml-2">
+        <button class="nav-link text-white" id="register" href="/register">ユーザ登録</button>
+      </li>
+      <li class="nav-item ml-2">
+        <button class="nav-link text-white" href="#">ログイン</button>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<style>
+  .navbar-laravel {
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.04);
+  }
+
+  button {
+    background-color: #008CFF;
+    border-radius: 100vh;
+  }
+
+  html {
+    font-family: sans-serif;
+    line-height: 1.15;
+  }
+
+  body {
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica Neue, Hiragino Kaku Gothic ProN, "メイリオ", meiryo, sans-serif;
+    font-weight: 400;
+  }
+
+  nav {
+    background-color: #CCCCCC !important;
+  }
+</style>
