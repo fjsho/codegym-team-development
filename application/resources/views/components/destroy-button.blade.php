1e8c2f6df10fa21b915b909e45468b177dbcd632
fjsho <gorimura7@gmail.com>
2022-03-09 17:58:20 +0900

    [fix]削除ボタンのデザイン修正

--- application/resources/views/components/destroy-button.blade.php
+++ application/resources/views/components/destroy-button.blade.php
@@ -1,4 +1,4 @@
 {{-- destroyボタン（仮） --}}
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-400 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-red-500 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-400 rounded-md font-semibold text-xs text-gray-400 tracking-widest hover:bg-gray-100 disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
