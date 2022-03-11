c73920c754d285d9f9f89d3b55d955bc029c4146
giyana <wktkktkr5@gmail.com>
2022-03-11 21:33:58 +0900

    typo修正

--- application/routes/web.php
+++ application/routes/web.php
@@ -16,11 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
 Route::get('/', [PostController::class, 'index'])
     ->name('dashboard');
 
