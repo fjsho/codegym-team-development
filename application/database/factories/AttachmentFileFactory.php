7cafeea4001041ee9f32730403d4baa86797285a
fjsho <gorimura7@gmail.com>
2022-03-14 21:58:07 +0900

    [fix]シーディングによるダミー画像保存及び保存用ディレクトリ生成処理の記述

--- application/database/factories/AttachmentFileFactory.php
+++ application/database/factories/AttachmentFileFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\AttachmentFile;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 class AttachmentFileFactory extends Factory
 {
@@ -21,8 +22,17 @@ class AttachmentFileFactory extends Factory
      */
     public function definition()
     {
+        //ダミーファイル保存用の記述
+        $storage_name = 'attachment_pic'; //ストレージとするディレクトリ名を記述する
+        $storage_dir_path = Storage::disk('local')->path('public/'.$storage_name);
+        if(Storage::disk('local')->missing('public/'.$storage_name)){
+            Storage::makeDirectory('public/'.$storage_name);
+        }
+        $picture = $this->faker->image($storage_dir_path, 620, 325, 'city', false);
+        $file_path = str_replace($storage_dir_path, '', $picture);
+
         return [
-        'attachment_pic_path' => $this->faker->url
+        'attachment_pic_path' => $file_path,
         ];
     }
 }
