ca5a0ed32c27ac56f1a9f5accff958f02a3e8951
masayoshixxx <satomasayoshi.xxx@gmail.com>
2022-03-23 20:43:42 +0900

    ボタン濃→淡へホバー色変更

--- application/resources/views/components/follow-button.blade.php
+++ application/resources/views/components/follow-button.blade.php
@@ -1,4 +1,4 @@
 {{-- フォローボタン --}}
-<button {{ $attributes->merge(['type' => 'submit',  'class' => 'inline-flex items-center px-4 py-2 bg-blue-400 border border-transparent rounded-full font-semibold text-xs text-white tracking-widest hover:bg-blue-500 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit',  'class' => 'inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-full font-semibold text-xs text-white tracking-widest hover:bg-blue-400 disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
