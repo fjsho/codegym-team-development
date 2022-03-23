e26b3e42a02e9b1e61b83009b0b27ecbce15e610
fjsho <gorimura7@gmail.com>
2022-03-23 18:06:32 +0900

    [fix]一時画像添付機能にバリデーション追加

--- application/app/Http/Controllers/TmpFileController.php
+++ application/app/Http/Controllers/TmpFileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Rules\AttachmentFileValidation;
 
 class TmpFileController extends Controller
 {
@@ -13,9 +14,11 @@ class TmpFileController extends Controller
      */
     public function storeTmpFile(Request $request)
     {
+        $request->validate([
+            'file' => ['required', 'file', new AttachmentFileValidation]
+        ]);
         //投稿予定画像がある場合、tmpディレクトリに画像を保存し、セッション情報を一時的に保持する
         if($file = $request->file('file')){
-            $tmp_file_name = $file->hashName();
             $tmp_file_path = basename($file->store('public/tmp'));
             $request->session()->put('tmp_file', $tmp_file_path);
 
