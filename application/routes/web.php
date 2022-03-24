0fce0d969c304e57605cb1299c482f5de219d35b
fjsho <gorimura7@gmail.com>
2022-03-24 11:58:23 +0900

    [fix]ルーティングの修正

--- application/routes/web.php
+++ application/routes/web.php
@@ -22,9 +22,8 @@ Route::get('/', [PostController::class, 'index'])
 
 require __DIR__.'/auth.php';
 
-Route::resource('users', UserController::class)->only([
-    'show','edit'
-]);
+Route::resource('users', UserController::class)
+    ->middleware(['auth']);
 
 //Post用
 Route::resource('posts', PostController::class)
