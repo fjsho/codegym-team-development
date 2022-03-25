c2be31bfc45df8b2260bebffc2c0f61fef143da2
fjsho <gorimura7@gmail.com>
2022-03-25 17:44:12 +0900

    Revert "Merge pull request #35 from fjsho/revert-34-release/ver1.0"
    This reverts commit c2452abf88ead533e4dccb5c33fb4b563d9f51f0, reversing
    changes made to cc31a3b6c77ea30609c5956298e06c958685a83d.

---
+++ application/resources/views/users/show.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex py-12">
+            <div class="flex-shrink-0 w-32 h-32">
+                <x-profile-pic src="{{ asset('storage/profile_pic/'.$profile->profile_pic_path)}}" />
+            </div>
+            <div class="w-full pt-4 pl-7">
+                <div class="flex justify-between">
+                    <div class="pb-6 text-xl">
+                        <p>{{$profile->name}}</p>
+                    </div>
+                    <div>
+                        @if ($user->id === $profile->id)
+                            <x-link-button-gray href="{{ route('users.edit', ['user' => $user->id])}}">プロフィール編集</x-link-button-gray>
+                        {{-- @TODO フォロー機能追加後、ボタンにそれぞれ機能を付与する --}}
+                        @elseif ($user->following->contains('id', $profile->id))
+                            <x-unfollow-button>フォロー中</x-unfollow-button>
+                        @else
+                            <x-follow-button>フォロー</x-follow-button>
+                        @endif
+                    </div>
+                </div>
+                <div class="text-sm">
+                    <p>{{$profile->self_introduction}}</p>
+                </div>
+            </div>
+        </div>
+    </x-slot>
+</x-app-layout>
